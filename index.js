40bd8891fa246fb114b92b57c6c4f7ab65756793
swati-3107 <pawarswati3107@gmail.com>
2024-02-09 19:00:43 +0530

    ui added

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config({ path: "./.env" });
+const path = require("path");
 
 mongoose.connect(process.env.MONGO_URL);
 const app = express();
 app.use(express.json());
 app.use(cors());
+// middleware
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.use("/api/admin", require("./routes/todoRoute"));
+
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 mongoose.connection.once("open", () => {
   console.log("MONGO CONNECTED");
